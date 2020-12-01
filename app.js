2ce03c989104d508b5f75bd234142c9dc2a4e54b
Sladyn Nunes <sladynnunes98@gmail.com>
2020-12-01 19:19:19 +0530

    ex05

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var textArea = document.querySelector("#txt-input")
+var outputDiv = document.querySelector("#output")
 
 btnTranslate.addEventListener("click", () => {
-    console.log("This is a button click")
-    console.log(textArea.value)
+   outputDiv.innerText = "Hello world"
 })
 
