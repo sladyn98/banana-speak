2557154127c3485b4fc2249f0d15779ebf4830da
Sladyn Nunes <sladynnunes98@gmail.com>
2020-12-01 22:45:32 +0530

    Complete project

--- app.js
+++ app.js
@@ -2,7 +2,21 @@ var btnTranslate = document.querySelector("#btn-translate");
 var textArea = document.querySelector("#txt-input")
 var outputDiv = document.querySelector("#output")
 
+
+function constructUrl(text) {
+  return  '	https://api.funtranslations.com/translate/minion.json' + "?" + "text=" + text
+}
+
+function callBackErrorHandler() {
+   console.log("Something went wrong with the api call")
+   alert("Sorry, something went wrong with the api call")
+}
 btnTranslate.addEventListener("click", () => {
-   outputDiv.innerText = "Hello world"
+   let inputText = textArea.value 
+   console.log("Input text is " + inputText)
+   fetch(constructUrl(inputText))
+   .then(response => response.json())
+   .then(json => outputDiv.innerText = json.contents.translated)
+   .catch(callBackErrorHandler)
 })
 
